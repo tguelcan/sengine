3c4fda17c8fbb494bc12f2af76c6ca950b91c557
Tayfun <tayfunmobile@gmail.com>
2025-01-24 19:08:38 +0100

    docs: update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,11 @@
 	{@render children()}
 	<footer class="container footer p-4 text-neutral-content mt-auto">
 		<aside>
-			<p>Copyright © {new Date().getFullYear()} - All right reserved.</p>
+			<p>Copyright © {new Date().getFullYear()} - All right reserved. MIT Licence</p>
 		</aside>
 		<nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+			<a href="https://github.com/tguelcan/sengine" target="_blank">GitHub</a>
+			<a href="https://github.com/tguelcan/sengine/blob/master/LICENSE" target="_blank">License (MIT)</a>
 			<a href="/legal">Legal notes</a>
 		</nav>
 	</footer>
